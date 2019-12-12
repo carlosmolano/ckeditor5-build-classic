99d5486303be108675c6de483a7b94e2e6bbfa4d
gvreddy04 <gvreddy04@gmail.com>
2019-12-12 11:52:19 +0530

    new pluggins enabled

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -12,6 +12,11 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -32,6 +37,7 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
 import Font from '@ckeditor/ckeditor5-font/src/font';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -43,6 +49,11 @@ ClassicEditor.builtinPlugins = [
 	Autoformat,
 	Bold,
 	Italic,
+	Underline,
+	Strikethrough,
+	Code,
+	Subscript,
+	Superscript,
 	BlockQuote,
 	CKFinder,
 	EasyImage,
@@ -62,7 +73,8 @@ ClassicEditor.builtinPlugins = [
 	TableToolbar,
 	Highlight,
 	HorizontalLine,
-	Font
+	Font,
+	CodeBlock
 ];
 
 // Editor configuration.
@@ -75,7 +87,11 @@ ClassicEditor.defaultConfig = {
             'fontfamily',
             '|',
             'bold',
-            'italic',
+			'italic',
+			'strikethrough',
+			'code',
+			'subscript',
+			'superscript',
             'highlight',
             '|',
             'fontcolor',
@@ -97,7 +113,9 @@ ClassicEditor.defaultConfig = {
             'removeformat',
             '|',
             'undo',
-            'redo',
+			'redo',
+			'|',
+			'codeBlock'
 		]
 	},
 	image: {
